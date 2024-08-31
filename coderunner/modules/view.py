eec370651217d2924ab100ba51446c2b007566d0
Danila Petrov <d.a.petrov08@gmail.com>
2024-08-31 20:11:30 +0300

    codemirror

--- coderunner/modules/view.py
+++ coderunner/modules/view.py
@@ -4,10 +4,14 @@ class ViewMixin:
   static_css = [
     'public/css/roboto.css',
     'public/css/reset.css',
+    'public/css/cm.css',
     'public/css/index.css',
   ]
   
   static_js = [
+    'public/js/cm.js',
+    'public/js/cm-al.js',
+    'public/js/cm-py.js',
     'public/js/index.js',
   ]
   
@@ -22,8 +26,7 @@ class ViewMixin:
         }
       )
     )
-    
-    
+
     for cssFilePath in self.static_css:
       frag.add_css(self.loader.load_unicode(cssFilePath))
       
--- coderunner/public/js/index.js
+++ coderunner/public/js/index.js
@@ -1,22 +1,39 @@
 /* Javascript for CodeRunnerXBlock. */
 function CodeRunnerXBlock(runtime, element) {
+  let editor;
 
-    function updateCount(result) {
-        $('.count', element).text(result.count);
-    }
+  function initCodeEditor() {
+    const codeEditor = CodeMirror.fromTextArea(
+      document.getElementById("code"),
+      {
+        styleActiveLine: true,
+        lineNumbers: true,
+        matchBrackets: true,
+        autoCloseBrackets: true,
+        autoCloseTags: true,
+        mode: "python",
+      }
+    );
 
-    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+    editor = codeEditor;
+  }
 
-    $('p', element).click(function(eventObject) {
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({"hello": "world"}),
-            success: updateCount
-        });
+  function updateCount(result) {
+    $(".count", element).text(result.count);
+  }
+
+  var handlerUrl = runtime.handlerUrl(element, "increment_count");
+
+  $("button.button", element).click(function (eventObject) {
+    $.ajax({
+      type: "POST",
+      url: handlerUrl,
+      data: JSON.stringify({ hello: "world" }),
+      success: updateCount,
     });
+  });
 
-    $(function ($) {
-        /* Here's where you'd do things on page load. */
-    });
+  $(function ($) {
+    initCodeEditor()
+  });
 }
